5c458e98f982565eb963b5fc1ef238b7451e9509
nutcracker <mrchenzhaojun@gmail.com>
2021-06-21 17:14:02 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'Base-Station-5de6178887b4473e927e08a2bbaf5771',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Base Station',
+  domain: 'niceloc.com',
+  author: 'Nutcracker Fischer',
 
   // open graph metadata (optional)
   description: 'Example site description',
